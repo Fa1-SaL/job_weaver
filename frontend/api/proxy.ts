3c27123f37de19d32021106e5b9ea3722e58a1e6
Mohd Faisal Alam <faisalalam73@gmail.com>
2026-08-18 13:26:19 +0530

    Fix Vercel proxy ESM import

--- frontend/api/proxy.ts
+++ frontend/api/proxy.ts
@@ -2,7 +2,7 @@ import {
   proxyRenderRequest,
   type ProxyRequest,
   type ProxyResponse
-} from '../server/renderProxy';
+} from '../server/renderProxy.js';
 
 export default async function handler(request: ProxyRequest, response: ProxyResponse) {
   await proxyRenderRequest(request, response);
